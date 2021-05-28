9f3e495b908743f24e312ec202f86c1d13eaf36a
nicolas.sutter <nicolas.sutter@epita.fr>
2021-05-28 17:00:58 +0200

    Api framework and testMethod

---
+++ lernejo-tester/src/test/java/SomeLernejoTests.java
@@ -0,0 +1,13 @@
+import fr.lernejo.tester.api.TestMethod;
+
+public class SomeLernejoTests {
+    @TestMethod
+    public void ok(){
+    }
+    @TestMethod
+    public void ko(){
+        throw new java.lang.IllegalStateException();
+    }
+    public void none(){
+    }
+}
